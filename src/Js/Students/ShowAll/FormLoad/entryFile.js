cc154916451b28e097f3029be9123a9d8bd0d800
keshavsoft <nknnkumar@yahoo.com>
2025-03-31 18:10:08 +0530

    common js file fetch get working

--- src/Js/Students/ShowAll/FormLoad/entryFile.js
+++ src/Js/Students/ShowAll/FormLoad/entryFile.js
@@ -1,8 +1,6 @@
 import { StartFunc as StartFuncAddListeners } from "./AddListeners/StartFunc.js";
-import { StartFunc as StartFuncFromFetchAsGet } from "./FetchAsGet/entryFile.js";
 
 const StartFunc = () => {
-    StartFuncFromFetchAsGet().then();
     StartFuncAddListeners();
 };
 
