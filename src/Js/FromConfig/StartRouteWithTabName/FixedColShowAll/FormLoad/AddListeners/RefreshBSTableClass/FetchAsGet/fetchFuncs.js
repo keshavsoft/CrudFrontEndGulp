99b27775575e48f0178a2a00cd48b7682d896bb6
keshavsoft <nknnkumar@yahoo.com>
2025-04-10 18:02:37 +0530

    FixedColShowAll working good

--- src/Js/FromConfig/StartRouteWithTabName/FixedColShowAll/FormLoad/AddListeners/RefreshBSTableClass/FetchAsGet/fetchFuncs.js
+++ src/Js/FromConfig/StartRouteWithTabName/FixedColShowAll/FormLoad/AddListeners/RefreshBSTableClass/FetchAsGet/fetchFuncs.js
@@ -1,9 +1,12 @@
 import getUrlJson from './getUrl.json' with {type: 'json'};
+import CommonConfigJson from '../../../../../CommonConfig.json' with {type: 'json'};
 
 let StartFunc = async () => {
     let jVarLocalFetchUrl = getUrlJson.GetEndPoint;
+    const jVarLocalRoutePath = CommonConfigJson.CommonRoutePath;
+    const jVarLocalTableName = CommonConfigJson.CommonTableName;
 
-    let response = await fetch(jVarLocalFetchUrl);
+    let response = await fetch(`/${jVarLocalRoutePath}/${jVarLocalTableName}/${jVarLocalFetchUrl}`);
 
     return await response;
 };
