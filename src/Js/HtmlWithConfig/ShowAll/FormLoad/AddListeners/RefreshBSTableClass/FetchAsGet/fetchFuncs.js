fae4a70f0ad6dd4028d40df15509574c8b7d0d24
pushpadamalanka82 <y*********@gmail.com>
2025-04-10 11:26:53 +0530

    some changes

--- src/Js/HtmlWithConfig/ShowAll/FormLoad/AddListeners/RefreshBSTableClass/FetchAsGet/fetchFuncs.js
+++ src/Js/HtmlWithConfig/ShowAll/FormLoad/AddListeners/RefreshBSTableClass/FetchAsGet/fetchFuncs.js
@@ -2,7 +2,7 @@ import CommonConfig from '../../../../../CommonConfig.json' with {type: 'json'};
 import getUrlJson from './getUrl.json' with {type: 'json'};
 
 let StartFunc = async () => {
-    let jVarLocalCommonConfig = CommonConfig.CommonRoutePath;
+    let jVarLocalCommonConfig = CommonConfig;
     let jVarLocalPath = getUrlJson.GetEndPoint;
     let jVarLocalFetchUrl = `/${jVarLocalCommonConfig}/${jVarLocalPath}`;
 
