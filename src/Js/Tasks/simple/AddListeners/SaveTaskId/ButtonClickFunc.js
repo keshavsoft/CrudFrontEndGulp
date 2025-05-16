7a970f2f33d12bf446a0b1a3256558c12f86bcaf
keshavsoft <nknnkumar@yahoo.com>
2025-05-16 18:40:11 +0530

    tasks simple started

---
+++ src/Js/Tasks/simple/AddListeners/SaveTaskId/ButtonClickFunc.js
@@ -0,0 +1,29 @@
+let StartFunc = () => {
+    let jVarLocalBody = {};
+    jVarLocalBody.title = jFLocalTopBarTaskId();
+    jVarLocalBody.start = jFLocalTopBarDateId();
+
+    console.log("jVarLocalBody : ", jVarLocalBody);
+    return jVarLocalBody;
+
+};
+
+let jFLocalTopBarTaskId = () => {
+    let jVarLocalTopBarTaskId = 'TopBarTaskId'
+    let jVarLocalHtmlId = document.getElementById(jVarLocalTopBarTaskId);
+
+    if (jVarLocalHtmlId === null === false) {
+        return jVarLocalHtmlId.value.trim();
+    };
+};
+
+let jFLocalTopBarDateId = () => {
+    let jVarLocalTopBarDateId = 'TopBarDateId'
+    let jVarLocalHtmlId = document.getElementById(jVarLocalTopBarDateId);
+
+    if (jVarLocalHtmlId === null === false) {
+        return jVarLocalHtmlId.value.trim();
+    };
+};
+
+export { StartFunc };
